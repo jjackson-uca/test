421cc75c20617bb2687d8269c2098455c50adfd8
Joseph Jackson <joejak93@gmail.com>
2024-04-03 19:04:48 -0500

    24-4-2 Changes

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="parent">
+	<div class="child">
+		<div class="gchild" style="flex-grow:8; border-width:0px; gap: 1px">
+			<div class="ggchild" style="border-width:0px; gap: 1px">
+				<div class="ggchild" style="flex-direction:column; border-width:0px; gap: 1px">
+					<div class="ggchild">top</div>
+					<div class="ggchild">top</div>
+				</div>
+				<div class="ggchild" style="flex-grow:4">top</div>
+			</div>
+			<div class="ggchild" style="border-width:0px; gap: 1px">
+				<div class="ggchild" style="flex-grow:2">top</div>
+				<div class="ggchild">top</div>
+				<div class="ggchild">top</div>
+				<div class="ggchild">top</div>
+			</div>
+		</div>
+		<div class="gchild"><p>TEXT HERE</p></div>
+	</div>
+	<div class="child">
+		<div class="gchild"><p>TEXT HERE</p></div>
+		<div class="gchild"><p>TEXT HERE</p></div>
+		<div class="gchild"><p>TEXT HERE</p></div>
+		<div class="gchild"><p>TEXT HERE</p></div>
+		<div class="gchild" style="border-width:0px; gap: 1px">
+			<div class="ggchild"><p>text</p></div>
+			<div class="ggchild" style="flex-grow:3"><p>text</p></div>
+		</div>
+	</div>
+	<div class="child">
+		<div class="gchild"><p>TEXT HERE</p></div>
+		<div class="gchild"><p>TEXT HERE</p></div>
+		<div class="gchild"><p>TEXT HERE</p></div>
+		<div class="gchild"><p>TEXT HERE</p></div>
+		<div class="gchild"><p>TEXT HERE</p></div>
+	</div>
+</div>
+
+<style>
+	div {
+		background-color: rgb(148, 148, 255);
+		border: solid;
+		font-family: custom-font, Verdana, Geneva, Tahoma, sans-serif;
+		font-size: 14pt;
+		margin: 0px;
+		padding: 0px;
+	}
+
+	.parent {
+		height: 1200px;
+        aspect-ratio: 9/16;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.child:first-child {
+		flex-grow: 2;
+	}
+
+	.child {
+		flex-grow: 1;
+		padding: 10px;
+		border-width: 10px;
+		margin: 10px;
+		display: flex;
+		gap: 1px;
+	}
+
+	.gchild {
+		flex-grow: 1;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.ggchild {
+		flex-grow: 1;
+		display: flex;
+	}
+</style>
---
+++ my-app/src/routes/neumorphic/+page.svelte
@@ -0,0 +1,106 @@
+<div class="circle" style="border-width:0px">
+    <div class="circle dropshad" style="margin: 0px">
+        <div class="circle dropshad" style="margin: 0px; align-content:center; justify-content:baseline">
+            <div style="display:flex; border-width:0px; gap: 5 %">
+                <div style="border-radius: .25em; aspect-ratio: .5/20; width: 1px"></div>
+                <div style="border-radius: .25em; aspect-ratio: 1/11; width: 1px"></div>
+
+            </div>
+        </div>  
+    </div>
+</div>
+
+<br />
+<pre style="rotate: 90deg; border:none; text-align: center;
+}">=</pre>
+
+
+<div class="ltr">
+	<div class="ttb">
+		<div class="ltr">
+			<div>
+				<input type="checkbox" />
+			</div>
+			<div><input type="checkbox" /></div>
+		</div>
+		<div class="ltr">
+			<div>
+				<input type="checkbox" />
+			</div>
+			<div><input type="checkbox" /></div>
+		</div>
+		<div class="ltr">
+			<div>
+				<input type="checkbox" />
+			</div>
+			<div><input type="checkbox" /></div>
+		</div>
+		<div>
+			<input type="button" value="" />
+		</div>
+		<div><input type="button" value="" /></div>
+	</div>
+	<div class="ttb">
+        <div>
+            <div class="circle"></div>
+        </div>
+        <div></div>
+        <div class="ttb">
+            <div class="circle" style="">
+                <div class="circle dropshad" style="margin: 0px">
+                    <div class="circle dropshad" style="margin: 0px; align-content:center; justify-content:baseline">
+                            <pre style="rotate: 90deg; border:none;">=</pre>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </div>
+	<div class="ttb">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div class="ltr">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div></div>
+    </div>
+</div>
+
+<style>
+	div {
+		border: solid;
+		padding: 10px;
+        gap: 4px;
+        border-radius: 2em;
+        flex-grow: 1;
+
+	}
+
+	.ltr {
+		display: flex;
+		flex-direction: row;
+        justify-content: space-evenly;
+
+	}
+
+
+	.ttb {
+		display: flex;
+		flex-direction: column;
+        justify-content: space-around;
+	}
+
+    .circle{
+        border-radius: 90%;
+        flex-grow: 0;
+        aspect-ratio: 1;
+    }
+
+    .dropshad{
+        border-width: 0px;
+        box-shadow: 1px 1px 2px gray, 1px 1px 2px grey;
+        box-shadow: inset 1px 1px 2px gray, 1px 1px 2px grey;
+    }
+</style>
